6456fc9c1ec21770829df5bd355d05e4aab781e8
Ibrahim Abu Arafeh <ibrahim.kaa@gmail.com>
2017-05-29 18:51:22 +0300

    Add files via upload

---
+++ CalculationWS/Backup/Default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="ConsumeWS._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        WELCOME TO WEB SERVICE CALCULATIONS</h2>
+<h2>
+        BY: OMAR HIDMI</h2>
+<p>
+        Please choose 2 numbers and a method to calculate ...</p>
+<table>
+    <tr>
+        <td>
+            Number 1</td>
+        <td>
+            <asp:TextBox ID="txtNumberOne" runat="server" 
+                ontextchanged="txtNumberOne_TextChanged"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Number 2</td>
+        <td>
+            <asp:TextBox ID="txtNumberTwo" runat="server"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Method</td>
+        <td>
+            <asp:DropDownList ID="ddlMethod" runat="server" Height="16px" Width="128px">
+                <asp:ListItem Selected="True">Choose ...</asp:ListItem>
+                <asp:ListItem>+</asp:ListItem>
+                <asp:ListItem>-</asp:ListItem>
+                <asp:ListItem>*</asp:ListItem>
+                <asp:ListItem>/</asp:ListItem>
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            &nbsp;</td>
+        <td>
+            <asp:Button ID="btnCalculate" runat="server" onclick="btnCalculate_Click" 
+                Text="Calculate" Width="128px" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Result</td>
+        <td>
+            <asp:Label ID="LabelResult" runat="server"></asp:Label>
+        </td>
+    </tr>
+</table>
+</asp:Content>
---
+++ CalculationWS/ConsumeWS/Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿
+
+private string GetResult(string firstNumber, string secondNumber, int function)
+    {
+        ServiceReference.CalculatorWebServiceSoapClient client = 
+            new ServiceReference.CalculatorWebServiceSoapClient();
+
+        int a, b;
+        string result = null;
+
+        erra.Text = "";
+        errb.Text = "";
+
+        if (!int.TryParse(firstNumber, out a))
+        {
+            erra.Text = "Must be a valid 32-bit integer!";
+            return "";
+        }
+
+        if (!int.TryParse(secondNumber, out b))
+        {
+            errb.Text = "Must be a valid 32-bit integer!";
+            return "";
+        }
+
+        try
+        {
+            switch (function)
+            {
+                case 0:
+                    result = firstNumber + " + " + secondNumber + 
+			" = " + client.Add(a, b);
+                    break;
+                case 1:
+                    result = firstNumber + " - " + secondNumber + " = "
+                    + client.Subtract(a, b);
+                    break;
+                case 2:
+                    result = firstNumber + " * " + secondNumber + " = " + client.Multiply(a, b);
+                    break;
+                case 3:
+                    result = firstNumber + " / " +
+                    secondNumber + " = " + client.Division(a, b);
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            LabelResult.ForeColor = System.Drawing.Color.Red;
+            result = "Cannot Divide by Zero!";
+        }
+        return result;
+    }
+
+protected void btnCalculate(object sender, EventArgs e)
+    {
+        LabelResult.ForeColor = System.Drawing.Color.Black;
+        LabelResult.Text = GetResult(txtNumberOne.Text,txtNumberTwo.Text,ddlMethod.SelectedIndex);
+    }
+
